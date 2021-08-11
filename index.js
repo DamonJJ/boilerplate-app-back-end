daeaeba93af702f66d321aa5e8a63e6206c597eb
DamonJJ <damon.jacoby@evereve.com>
2021-08-11 09:46:25 -0500

    Updates index to match code in tutorial

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const Note = require('./models/note');
-const PORT = process.env.PORT;
 
+app.use(express.static('build'));
 app.use(cors());
-app.use(express.static('build'))
-app.use(express.json())
-// app.use(requestLogger) // request.body is undefined!
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
-});
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -54,29 +52,53 @@ app.post('/api/notes', (request, response) => {
   });
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log('WAT IS THIS');
+      next(error);
+    });
+});
 
-  response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-// handler of requests with unknown endpoint
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  }
-
-  next(error);
+  response.status(404).send({ error: 'unknown endpoint' });
 };
 
-// this has to be the last loaded middleware.
+app.use(unknownEndpoint);
+
 app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
