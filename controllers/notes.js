adf33411217dc0324c90a970dadbce3e29edaaf5
DamonJJ <damon.jacoby@evereve.com>
2021-09-02 11:14:16 -0500

    uibuild

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
@@ -8,10 +9,22 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes.map((note) => note.toJSON()));
 });
 
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body;
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
-  const user = await User.findById(body.userId);
+notesRouter.post('/', async (request, response) => {
+  const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
@@ -26,7 +39,6 @@ notesRouter.post('/', async (request, response, next) => {
 
   response.json(savedNote.toJSON());
 });
-
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id);
   if (note) {
